bdabfd526e9190272aa6cac828193ba3ffe3ddb5
Daniel Shiffman <daniel@shiffman.net>
2012-10-12 16:22:34 -0400

    adding new OF 0072 examples

---
+++ OF/xcode_src/0072/BigScreensMasterPlayer/src/testApp.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+#include "ofxXmlSettings.h"
+
+#define HOST "localhost"
+#define PORT 12345
+
+//--------------------------------------------------------
+class testApp : public ofBaseApp {
+
+	public:
+
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		void loadSettings(string fileString);
+
+
+	ofVideoPlayer 		movie;
+
+	
+	int count;
+	int howOften;
+	
+	bool started;
+	bool fullscreen;
+	
+	char * movieFile;
+	char * host1;
+	char * host2;
+	int port1;
+	int port2;
+	
+	int movieWidth;
+	int movieHeight;
+	int movieX;
+	int movieY;
+	
+	bool looping;
+	
+	bool                frameByframe;
+	
+	
+	ofxOscSender sender1;
+	ofxOscSender sender2;
+};
+
